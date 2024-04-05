3e7b49d98e1c0bf0b3ffd781b8957fcbc1582bed
Brian <bstanton773@gmail.com>
2024-04-05 12:32:03 -0500

    Display brewery data in table (first 10 only)

--- static/js/index.js
+++ static/js/index.js
@@ -74,8 +74,62 @@ function findBreweries(e){
     // Make the HTTP get request to the above url and log the data
     fetch(url)
         .then( res => res.json() )
-        .then( data => console.log(data) )
+        .then( data => displayBreweries(data) )
         .catch( err => console.error(err) )
 
 }
 
+
+// Callback function for findBreweries that will accept brewery data and insert into the display table
+function displayBreweries(data){
+    // Get the table from the HTML
+    let table = document.getElementById('brewery-table');
+
+    // Clear out the table of any current data
+    table.innerHTML = '';
+
+    if (!data.length){
+        table.innerHTML = '<h1>No Breweries Here</h1>'
+        return
+    }
+
+    // Set up table headers
+    const thead = document.createElement('thead');
+    table.append(thead); // Add the thead as a child to the table
+    let tr = document.createElement('tr');
+    thead.append(tr); // add the table row as a child the table header
+    const tableHeadings = ['Name', 'Type', 'Street Address', 'Address 2', 'Address 3', 'City', 'State'];
+    tableHeadings.forEach( heading => {
+        let th = document.createElement('th');
+        th.scope = 'col';
+        th.innerHTML = heading;
+        tr.append(th)
+    } );
+
+    // Create the table body and populate with brewery data
+    let tbody = document.createElement('tbody');
+    table.append(tbody);
+
+    // Write a row for each brewery in data
+    for (let brewery of data){
+        let tr = document.createElement('tr');
+        tbody.append(tr);
+
+        newDataCell(tr, `<a href=${brewery.website_url} target="_blank">${brewery.name}</a>`)
+        newDataCell(tr, brewery.brewery_type);
+        newDataCell(tr, brewery.street);
+        newDataCell(tr, brewery.address_2);
+        newDataCell(tr, brewery.address_3);
+        newDataCell(tr, brewery.city);
+        newDataCell(tr, brewery.state);
+    }
+
+}
+
+
+// Helper Function for creating a new data cell for a table row
+function newDataCell(tr, value){
+    let td = document.createElement('td');
+    td.innerHTML = value ?? '-'
+    tr.append(td)
+}
